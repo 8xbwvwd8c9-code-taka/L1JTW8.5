86cd593f2b9e310e1d0dbfd34e76baea32540424
Takanashi <8xbwvwd8c9@privaterelay.appleid.com>
2026-09-22 16:39:45 +0800

    BUG-850-166 promote atomic clan transaction

--- recovery/normalized-src-vf/l1r/ao/ClanTable.java
+++ recovery/normalized-src-vf/l1r/ao/ClanTable.java
@@ -9,6 +9,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import l1r.ai.IdFactory;
+import l1r.ap.L1ItemInstance;
 import l1r.ap.L1PcInstance;
 import l1r.aq.L1Clan;
 import l1r.be.S_PacketBox;
@@ -148,6 +149,167 @@ public class ClanTable {
       var12.a(var1.et());
       var1.I();
       return var12;
+   }
+
+   public L1Clan createClanAtomic(L1PcInstance var1, String var2) {
+      if (var1 == null || var2 == null || var2.length() == 0) {
+         return null;
+      }
+
+      synchronized (var1) {
+         synchronized (var1.j()) {
+            if (var1.aF() != 0) {
+               return null;
+            }
+
+            for (L1Clan var3 : this.b.values()) {
+               if (var3.f().equalsIgnoreCase(var2)) {
+                  return null;
+               }
+            }
+
+            L1ItemInstance var4 = var1.j().b(40308);
+            if (var4 == null || var4.E() < 30000) {
+               return null;
+            }
+
+            int var5 = var4.E();
+            int var6 = var5 - 30000;
+            L1Clan var7 = new L1Clan();
+            var7.c(IdFactory.a().d());
+            var7.e(var2);
+            var7.f(var1.fr());
+            var7.g(var1.et());
+            var7.g(0);
+            var7.h(0);
+            var7.a(new Timestamp(System.currentTimeMillis()));
+            var7.f("");
+            var7.d(0);
+            var7.e(0);
+
+            Connection var8 = null;
+            boolean var9 = true;
+            boolean var10 = false;
+
+            try {
+               var8 = DatabaseFactory.a().b();
+               this.requireClanCreateInnoDb(var8);
+               var9 = var8.getAutoCommit();
+               var8.setAutoCommit(false);
+
+               try (PreparedStatement var11 = var8.prepareStatement(
+                  "INSERT INTO clan_data SET clan_id=?, clan_name=?, leader_id=?, leader_name=?, hascastle=?, hashouse=?, found_date=?, announcement=?, emblem_id=?, emblem_status=?"
+               )) {
+                  var11.setInt(1, var7.e());
+                  var11.setString(2, var7.f());
+                  var11.setInt(3, var7.k());
+                  var11.setString(4, var7.l());
+                  var11.setInt(5, var7.m());
+                  var11.setInt(6, var7.n());
+                  var11.setTimestamp(7, var7.g());
+                  var11.setString(8, "");
+                  var11.setInt(9, 0);
+                  var11.setInt(10, 0);
+                  if (var11.executeUpdate() != 1) {
+                     throw new SQLException("BUG-850-166 clan_data insert failed");
+                  }
+               }
+
+               try (PreparedStatement var12 = var8.prepareStatement(
+                  "INSERT INTO clan_members SET clan_id=?, char_id=?, char_name=?, date=?, notes=?"
+               )) {
+                  var12.setInt(1, var7.e());
+                  var12.setInt(2, var1.fr());
+                  var12.setString(3, var1.et());
+                  var12.setDate(4, new java.sql.Date(System.currentTimeMillis()));
+                  var12.setString(5, "");
+                  if (var12.executeUpdate() != 1) {
+                     throw new SQLException("BUG-850-166 clan_members insert failed");
+                  }
+               }
+
+               try (PreparedStatement var13 = var8.prepareStatement(
+                  "UPDATE characters SET ClanID=?, Clanname=?, ClanRank=? WHERE objid=? AND ClanID=0"
+               )) {
+                  var13.setInt(1, var7.e());
+                  var13.setString(2, var7.f());
+                  var13.setInt(3, 10);
+                  var13.setInt(4, var1.fr());
+                  if (var13.executeUpdate() != 1) {
+                     throw new SQLException("BUG-850-166 character clan CAS failed");
+                  }
+               }
+
+               CharacterItemTable var14 = CharacterItemTable.a();
+               if (var6 == 0) {
+                  var14.deleteQuestRewardItem(var8, var1.fr(), var4, var5);
+               } else {
+                  var14.updateQuestRewardCount(var8, var1.fr(), var4, var5, var6);
+               }
+
+               var8.commit();
+               var10 = true;
+            } catch (Exception var18) {
+               if (var8 != null) {
+                  try {
+                     var8.rollback();
+                  } catch (SQLException var17) {
+                     a.log(Level.SEVERE, var17.getLocalizedMessage(), var17);
+                  }
+               }
+
+               a.log(Level.SEVERE, "BUG-850-166 clan creation transaction failed", var18);
+            } finally {
+               if (var8 != null) {
+                  try {
+                     var8.setAutoCommit(var9);
+                  } catch (SQLException var16) {
+                     a.log(Level.SEVERE, var16.getLocalizedMessage(), var16);
+                  }
+               }
+
+               SQLUtil.a(var8);
+            }
+
+            if (!var10) {
+               return null;
+            }
+
+            this.b.put(var7.e(), var7);
+            var1.ah(var7.e());
+            var1.c(var7.f());
+            var1.ai(10);
+            var1.a(new S_PacketBox(27, 10, var1.et()));
+            var7.a(var1.et());
+
+            if (var6 == 0) {
+               var1.j().publishCommittedQuestDelete(var4);
+            } else {
+               var1.j().publishCommittedQuestUpdate(var4, var6);
+            }
+
+            return var7;
+         }
+      }
+   }
+
+   private void requireClanCreateInnoDb(Connection var1) throws SQLException {
+      try (PreparedStatement var2 = var1.prepareStatement(
+         "SELECT TABLE_NAME, ENGINE FROM information_schema.TABLES WHERE TABLE_SCHEMA=DATABASE() AND TABLE_NAME IN ('clan_data','clan_members','characters','character_items')"
+      );
+           ResultSet var3 = var2.executeQuery()) {
+         int var4 = 0;
+         while (var3.next()) {
+            if (!"InnoDB".equalsIgnoreCase(var3.getString("ENGINE"))) {
+               throw new SQLException("BUG-850-166 requires InnoDB clan creation tables");
+            }
+            var4++;
+         }
+
+         if (var4 != 4) {
+            throw new SQLException("BUG-850-166 missing clan creation transaction table");
+         }
+      }
    }
 
    public void b(L1Clan var1) {
