6bbf97194fe6dd80a0b7f18527f8f0b9fd51e7f0
Takanashi <8xbwvwd8c9@privaterelay.appleid.com>
2026-09-22 18:09:10 +0800

    BUG-850-145/146 promote normalized atomic house settlement

--- recovery/normalized-src-vf/l1r/ba/HouseTimer.java
+++ recovery/normalized-src-vf/l1r/ba/HouseTimer.java
@@ -53,19 +53,21 @@ public class HouseTimer {
       int var4 = var1.m();
       String var5 = var1.n();
       int var6 = var1.o();
-      L1Clan var14 = var6 == 0 ? null : this.findEligibleBidderClan(var6);
-      if (var6 != 0 && var14 == null) {
+      L1Clan var17 = this.findOwnerClan(var2);
+      L1Clan var18 = var6 == 0 ? null : this.findEligibleBidderClan(var6);
+
+      if (var6 != 0 && var18 == null) {
          if (var3 > 0 && !this.refundBidder(var6, var3)) {
             return;
          }
 
-         String var15 = var1.n();
-         int var16 = var1.o();
+         String var19 = var1.n();
+         int var20 = var1.o();
          var1.d("");
          var1.f(0);
          if (!this.a((L1Clan)null, var1)) {
-            var1.d(var15);
-            var1.f(var16);
+            var1.d(var19);
+            var1.f(var20);
             if (var3 > 0) {
                this.reclaimBidderRefund(var6, var3);
             }
@@ -74,73 +76,199 @@ public class HouseTimer {
       }
 
       if (var4 != 0 && var6 != 0) {
-         L1PcInstance var12 = (L1PcInstance)L1World.a().a(var4);
          int var8 = (int)(var3 * 0.9);
-         if (var12 != null) {
-            ItemTable.a(var12, 40308, var8);
-            var12.a(new S_ServerMessage(527, String.valueOf(var8)));
-         } else {
-            L1ItemInstance var9 = ItemTable.a().b(40308);
-            var9.e(var8);
-            CharacterItemTable.a().a(var4, var9);
+         if (var8 <= 0 || !this.refundBidder(var4, var8)) {
+            return;
          }
 
-         L1PcInstance var13 = (L1PcInstance)L1World.a().a(var6);
+         int var9 = var17 == null ? 0 : var17.n();
+         int var10 = var18.n();
+         boolean var11 = var1.g();
+         Timestamp var12 = var1.i();
+
+         if (var17 != null) {
+            var17.h(0);
+         }
+         var18.h(var2);
+         var1.a(false);
+         var1.a(new Timestamp(System.currentTimeMillis() + Config.an * 24 * 60 * 60 * 1000L));
+
+         if (!this.settleAtomic(var17, var18, var1)) {
+            if (var17 != null) {
+               var17.h(var9);
+            }
+            var18.h(var10);
+            var1.a(var11);
+            var1.a(var12);
+            this.reclaimBidderRefund(var4, var8);
+            return;
+         }
+
+         L1PcInstance var13 = (L1PcInstance)L1World.a().a(var4);
          if (var13 != null) {
-            var13.a(new S_ServerMessage(524, String.valueOf(var3), var5));
+            var13.a(new S_ServerMessage(527, String.valueOf(var8)));
          }
 
-         this.a(var2);
-         this.a(var2, var6);
-         this.b(var2);
-      } else if (var4 == 0 && var6 != 0) {
-         L1PcInstance var11 = (L1PcInstance)L1World.a().a(var6);
-         if (var11 != null) {
-            var11.a(new S_ServerMessage(524, String.valueOf(var3), var5));
+         L1PcInstance var14 = (L1PcInstance)L1World.a().a(var6);
+         if (var14 != null) {
+            var14.a(new S_ServerMessage(524, String.valueOf(var3), var5));
+         }
+         return;
+      }
+
+      if (var4 == 0 && var6 != 0) {
+         int var15 = var18.n();
+         boolean var16 = var1.g();
+         Timestamp var21 = var1.i();
+
+         var18.h(var2);
+         var1.a(false);
+         var1.a(new Timestamp(System.currentTimeMillis() + Config.an * 24 * 60 * 60 * 1000L));
+
+         if (!this.settleAtomic(null, var18, var1)) {
+            var18.h(var15);
+            var1.a(var16);
+            var1.a(var21);
+            return;
          }
 
-         this.a(var2, var6);
-         this.b(var2);
-      } else if (var4 != 0 && var6 == 0) {
-         L1PcInstance var10 = (L1PcInstance)L1World.a().a(var4);
-         if (var10 != null) {
-            var10.a(new S_ServerMessage(528));
+         L1PcInstance var22 = (L1PcInstance)L1World.a().a(var6);
+         if (var22 != null) {
+            var22.a(new S_ServerMessage(524, String.valueOf(var3), var5));
+         }
+         return;
+      }
+
+      if (var4 != 0) {
+         boolean var23 = var1.g();
+         Timestamp var24 = var1.i();
+         var1.a(false);
+         var1.a(new Timestamp(System.currentTimeMillis() + Config.an * 24 * 60 * 60 * 1000L));
+
+         if (!this.settleAtomic(null, null, var1)) {
+            var1.a(var23);
+            var1.a(var24);
+            return;
          }
 
-         this.b(var2);
-      } else if (var4 == 0 && var6 == 0) {
-         Timestamp var7 = new Timestamp(System.currentTimeMillis() + 432000000L);
-         var1.b(var7);
-         var1.d(100000);
-         HouseTable.a().a(var1);
+         L1PcInstance var25 = (L1PcInstance)L1World.a().a(var4);
+         if (var25 != null) {
+            var25.a(new S_ServerMessage(528));
+         }
+         return;
+      }
+
+      Timestamp var26 = var1.j();
+      int var27 = var1.k();
+      var1.b(new Timestamp(System.currentTimeMillis() + 432000000L));
+      var1.d(100000);
+      if (!this.settleAtomic(null, null, var1)) {
+         var1.b(var26);
+         var1.d(var27);
       }
    }
 
-   private void a(int var1) {
+   private L1Clan findOwnerClan(int var1) {
       for (L1Clan var2 : ClanTable.a().b().values()) {
          if (var2.n() == var1) {
-            var2.h(0);
-            ClanTable.a().b(var2);
+            return var2;
          }
       }
+
+      return null;
    }
 
-   private void a(int var1, int var2) {
-      for (L1Clan var3 : ClanTable.a().b().values()) {
-         if (var3.k() == var2) {
-            var3.h(var1);
-            ClanTable.a().b(var3);
-            break;
+   private boolean settleAtomic(L1Clan var1, L1Clan var2, L1House var3) {
+      Connection var4 = null;
+      PreparedStatement var5 = null;
+      PreparedStatement var6 = null;
+      PreparedStatement var7 = null;
+
+      try {
+         var4 = l1r.l1j.server.DatabaseFactory.a().b();
+         var4.setAutoCommit(false);
+
+         if (var1 != null) {
+            var5 = var4.prepareStatement("UPDATE clan_data SET hashouse=? WHERE clan_name=?");
+            var5.setInt(1, var1.n());
+            var5.setString(2, var1.f());
+            if (var5.executeUpdate() != 1) {
+               throw new SQLException("BUG-850-145 old clan ownership update failed");
+            }
+         }
+
+         if (var2 != null) {
+            var6 = var4.prepareStatement("UPDATE clan_data SET hashouse=? WHERE clan_name=?");
+            var6.setInt(1, var2.n());
+            var6.setString(2, var2.f());
+            if (var6.executeUpdate() != 1) {
+               throw new SQLException("BUG-850-145 bidder clan ownership update failed");
+            }
+         }
+
+         var7 = var4.prepareStatement(
+            "UPDATE house SET house_name=?, house_area=?, location=?, keeper_id=?, is_on_sale=?, is_purchase_basement=?, tax_deadline=?, deadline=?, price=?, old_owner=?, old_owner_id=?, bidder=?, bidder_id=? WHERE house_id=?"
+         );
+         var7.setString(1, var3.c());
+         var7.setInt(2, var3.d());
+         var7.setString(3, var3.e());
+         var7.setInt(4, var3.f());
+         var7.setBoolean(5, var3.g());
+         var7.setBoolean(6, var3.h());
+         var7.setTimestamp(7, var3.i());
+         var7.setTimestamp(8, var3.j());
+         var7.setInt(9, var3.k());
+         var7.setString(10, var3.l());
+         var7.setInt(11, var3.m());
+         var7.setString(12, var3.n());
+         var7.setInt(13, var3.o());
+         var7.setInt(14, var3.b());
+         if (var7.executeUpdate() != 1) {
+            throw new SQLException("BUG-850-145 house settlement update failed");
+         }
+
+         var4.commit();
+         return true;
+      } catch (SQLException var11) {
+         a.log(Level.SEVERE, var11.getLocalizedMessage(), var11);
+         if (var4 != null) {
+            try {
+               var4.rollback();
+            } catch (SQLException var10) {
+               a.log(Level.SEVERE, var10.getLocalizedMessage(), var10);
+            }
+         }
+         return false;
+      } finally {
+         if (var7 != null) {
+            try {
+               var7.close();
+            } catch (SQLException ignored) {
+            }
+         }
+         if (var6 != null) {
+            try {
+               var6.close();
+            } catch (SQLException ignored) {
+            }
+         }
+         if (var5 != null) {
+            try {
+               var5.close();
+            } catch (SQLException ignored) {
+            }
+         }
+         if (var4 != null) {
+            try {
+               var4.setAutoCommit(true);
+            } catch (SQLException ignored) {
+            }
+            try {
+               var4.close();
+            } catch (SQLException ignored) {
+            }
          }
       }
-   }
-
-   private void b(int var1) {
-      L1House var2 = HouseTable.a().a(var1);
-      var2.a(false);
-      Timestamp var3 = new Timestamp(System.currentTimeMillis() + Config.an * 24 * 60 * 60 * 1000L);
-      var2.a(var3);
-      HouseTable.a().a(var2);
    }
 
    private void d() {
