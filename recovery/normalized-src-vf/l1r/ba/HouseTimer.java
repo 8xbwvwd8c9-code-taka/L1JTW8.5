9bfc4bb8ee99d365e8bd8c905d6f6632997b61a9
Takanashi <8xbwvwd8c9@privaterelay.appleid.com>
2026-09-22 17:52:09 +0800

    BUG-850-149 promote normalized atomic foreclosure

--- recovery/normalized-src-vf/l1r/ba/HouseTimer.java
+++ recovery/normalized-src-vf/l1r/ba/HouseTimer.java
@@ -1,5 +1,8 @@
 package l1r.ba;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.TimerTask;
 import java.util.logging.Level;
@@ -131,16 +134,31 @@ public class HouseTimer {
 
    public void a(L1House var1) {
       int var2 = var1.b();
+      L1Clan var3 = null;
 
-      for (L1Clan var3 : ClanTable.a().b().values()) {
-         if (var3.n() == var2) {
-            var3.h(0);
-            ClanTable.a().b(var3);
+      for (L1Clan var4 : ClanTable.a().b().values()) {
+         if (var4.n() == var2) {
+            var3 = var4;
+            break;
          }
       }
 
-      Timestamp var5 = new Timestamp(System.currentTimeMillis() + 432000000L);
-      var1.b(var5);
+      Timestamp var5 = var1.j();
+      int var6 = var1.k();
+      String var7 = var1.l();
+      int var8 = var1.m();
+      String var9 = var1.n();
+      int var10 = var1.o();
+      boolean var11 = var1.g();
+      boolean var12 = var1.h();
+      Timestamp var13 = var1.i();
+      int var14 = var3 == null ? 0 : var3.n();
+
+      if (var3 != null) {
+         var3.h(0);
+      }
+
+      var1.b(new Timestamp(System.currentTimeMillis() + 432000000L));
       var1.d(100000);
       var1.c("");
       var1.e(0);
@@ -148,10 +166,100 @@ public class HouseTimer {
       var1.f(0);
       var1.a(true);
       var1.b(false);
-      Timestamp var6 = new Timestamp(System.currentTimeMillis() + Config.an * 24 * 60 * 60 * 1000L);
-      var1.a(var6);
+      var1.a(new Timestamp(System.currentTimeMillis() + Config.an * 24 * 60 * 60 * 1000L));
       var1.a();
-      HouseTable.a().a(var1);
+
+      if (!this.a(var3, var1)) {
+         if (var3 != null) {
+            var3.h(var14);
+         }
+
+         var1.b(var5);
+         var1.d(var6);
+         var1.c(var7);
+         var1.e(var8);
+         var1.d(var9);
+         var1.f(var10);
+         var1.a(var11);
+         var1.b(var12);
+         var1.a(var13);
+      }
+   }
+
+   private boolean a(L1Clan var1, L1House var2) {
+      Connection var3 = null;
+      PreparedStatement var4 = null;
+      PreparedStatement var5 = null;
+
+      try {
+         var3 = l1r.l1j.server.DatabaseFactory.a().b();
+         var3.setAutoCommit(false);
+         if (var1 != null) {
+            var4 = var3.prepareStatement("UPDATE clan_data SET hashouse=? WHERE clan_name=?");
+            var4.setInt(1, var1.n());
+            var4.setString(2, var1.f());
+            if (var4.executeUpdate() != 1) {
+               throw new SQLException("BUG-850-149 clan ownership update failed");
+            }
+         }
+
+         var5 = var3.prepareStatement(
+            "UPDATE house SET house_name=?, house_area=?, location=?, keeper_id=?, is_on_sale=?, is_purchase_basement=?, tax_deadline=?, deadline=?, price=?, old_owner=?, old_owner_id=?, bidder=?, bidder_id=? WHERE house_id=?"
+         );
+         var5.setString(1, var2.c());
+         var5.setInt(2, var2.d());
+         var5.setString(3, var2.e());
+         var5.setInt(4, var2.f());
+         var5.setBoolean(5, var2.g());
+         var5.setBoolean(6, var2.h());
+         var5.setTimestamp(7, var2.i());
+         var5.setTimestamp(8, var2.j());
+         var5.setInt(9, var2.k());
+         var5.setString(10, var2.l());
+         var5.setInt(11, var2.m());
+         var5.setString(12, var2.n());
+         var5.setInt(13, var2.o());
+         var5.setInt(14, var2.b());
+         if (var5.executeUpdate() != 1) {
+            throw new SQLException("BUG-850-149 house foreclosure update failed");
+         }
+
+         var3.commit();
+         return true;
+      } catch (SQLException var9) {
+         a.log(Level.SEVERE, var9.getLocalizedMessage(), var9);
+         if (var3 != null) {
+            try {
+               var3.rollback();
+            } catch (SQLException var8) {
+               a.log(Level.SEVERE, var8.getLocalizedMessage(), var8);
+            }
+         }
+         return false;
+      } finally {
+         if (var5 != null) {
+            try {
+               var5.close();
+            } catch (SQLException ignored) {
+            }
+         }
+         if (var4 != null) {
+            try {
+               var4.close();
+            } catch (SQLException ignored) {
+            }
+         }
+         if (var3 != null) {
+            try {
+               var3.setAutoCommit(true);
+            } catch (SQLException ignored) {
+            }
+            try {
+               var3.close();
+            } catch (SQLException ignored) {
+            }
+         }
+      }
    }
 
    private class L1R_a extends TimerTask {
