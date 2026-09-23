e2f87a758718cdfa384e332a150902f2cab9c99b
Takanashi <8xbwvwd8c9@privaterelay.appleid.com>
2026-09-23 11:05:18 +0800

    feat: surface dynamic HP MP probe in developer mode

--- launcher/850Launcher/Program.cs
+++ launcher/850Launcher/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace L1JTW850Launcher
@@ -16,7 +17,26 @@ namespace L1JTW850Launcher
                 var appDir = AppDomain.CurrentDomain.BaseDirectory;
                 var config = LauncherConfig.Load(System.IO.Path.Combine(appDir, "launcher.ini"));
                 var helper = HelperSettings.Load(System.IO.Path.Combine(appDir, "helper.ini"));
-                Application.Run(new MainForm(appDir, config, helper));
+                var main = new MainForm(appDir, config, helper);
+
+                if (config.DeveloperMode)
+                {
+                    main.Shown += delegate
+                    {
+                        var dynamicProbe = new Form
+                        {
+                            Text = "850 自動動態 HP/MP 偵測",
+                            Width = 820,
+                            Height = 520,
+                            MinimumSize = new Size(760, 460),
+                            StartPosition = FormStartPosition.CenterParent
+                        };
+                        dynamicProbe.Controls.Add(new RuntimeDynamicProbeControl(appDir));
+                        dynamicProbe.Show(main);
+                    };
+                }
+
+                Application.Run(main);
             }
             catch (Exception ex)
             {
